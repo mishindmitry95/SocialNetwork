915b72f0c8b2b059be6ad9b9525d58a524c3b4db
Mishin Dmitry <dima_mishin_95@mail.ru>
2020-04-05 12:35:19 +0300

    initial commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Profile = () => {
+	return (
+		<div className="content">
+			<div><img src="" alt="content_photo" /></div>
+			<div>ava + desc</div>
+			<div>
+				my posts
+				<div>new post</div>
+			</div>
+			<div>
+				<div>post1</div>
+				<div>post2</div>
+			</div>
+		</div>
+	);
+}
+
+export default Profile;
